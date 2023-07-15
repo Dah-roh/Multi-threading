0a09055dd4133c65dc850c44193870afd4cf2afb
daro-decagon <gadibiao@decagonhq.com>
2023-07-15 21:40:41 +0100

    initial commit

---
+++ src/main/java/org/example/MultiThreading3.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class MultiThreading3{
+
+    public void addNumber() {
+        Runnable runnable = ()->{
+            for (int i = 0; i < 100; i++) {
+                Multithreading.totalValue.getAndAdd(1);
+                Multithreading.totalValue.set(Multithreading.totalValue.get());
+                System.out.println("Thread number "+3+"="+Multithreading.totalValue.get());
+            }
+
+            System.out.println("Total value = "+Multithreading.totalValue.get());
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+}
---
+++ src/main/java/org/example/Multithreading.java
@@ -0,0 +1,47 @@
+package org.example;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+//CRON JOBS: These unit processes that are tasked to carry out a behaviour/logic after a set period
+//or to repeat a task at set intervals. Cron job basically help us with Scheduling.
+public class Multithreading extends Thread{
+    static AtomicInteger totalValue = new AtomicInteger(0);
+
+    private int threadNo;
+
+    public int getThreadNo() {
+        return threadNo;
+    }
+
+    public void setThreadNo(int threadNo) {
+        this.threadNo = threadNo;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+        totalValue.getAndAdd(1);
+        totalValue.set(totalValue.get());
+    }
+    }
+    public static void main(String[] args) {
+
+
+        Multithreading multithreading = new Multithreading();
+        Multithreading multithreading1 = new Multithreading();
+        MultiThreading3 multiThreading3 = new MultiThreading3();
+        multithreading.setThreadNo(1);
+        multithreading1.setThreadNo(2);
+        DeadLock deadLock =  new DeadLock();
+        deadLock.setThreadNo(4);
+
+        multithreading.start();
+        multithreading1.start();
+        multiThreading3.addNumber();
+        deadLock.start();
+
+
+    }
+
+}
